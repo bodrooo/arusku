a1f8e48eff77fc3a527bd7706986883241de3f32
bodrooo <bodroadikoro345@gmail.com>
2025-08-06 10:50:12 +0700

    feat: create dashboard page

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+
+    <main>
+        <h1>Dashboard Page</h1>
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@ Route::get('/register', function () {
     return Inertia::render('Register');
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard/Index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 require __DIR__ . '/auth.php';
